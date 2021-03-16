926e0da42aba57408a53d917933405bf654fe4c5
Samúel Jón Gunnarsson <samuel.jon.gunnarsson@gmail.com>
2021-03-16 23:06:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Breyttu kóðanum undir <code>src/App.js</code> og ýttu á vista til að endurhlaða.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Læra React
         </a>
       </header>
     </div>
